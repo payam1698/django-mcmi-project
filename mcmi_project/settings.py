d17e07389a70469139a41eaf0a7425fb897604cf
payam1698 <m.esmaeeilzadeh@gmail.com>
2024-12-26 16:20:38 +0330

    Initial commit: Add Django project files

---
+++ mcmi_project/settings.py
@@ -0,0 +1,25 @@
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+DEBUG = True
+ROOT_URLCONF = 'mcmi_project.urls'
+from decouple import config
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'mcmi_app',  # اپلیکیشن اصلی شما
+]
+
